2f03bd000c4c28d78a42f6220911895638d5ed55
el10savio <el10savio96@gmail.com>
2021-10-25 14:21:58 +0530

    Refactored To Table Driven Tests

--- lrucache/lrucache_test.go
+++ lrucache/lrucache_test.go
@@ -7,55 +7,54 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func createCache(capacity int) *Cache {
+	if capacity == 0 {
+		return nil
+	}
+
+	return &Cache{
+		Capacity:  capacity,
+		HashMap:   make(map[string]*CacheNode, capacity),
+		firstNode: nil,
+		lastNode:  nil,
+	}
+}
+
 func TestClear(t *testing.T) {
-	cache, err := LRUCache(2)
+	capacity := 2
+
+	expectedCache := createCache(capacity)
+
+	actualCache, err := LRUCache(2)
 	if err != nil {
 		t.Fatalf("Error in creating cache: %v", err)
 	}
 
-	cache.Set("0", "a")
-	cache.Set("1", "b")
-	cache.Clear()
-
-	expectedCache := &Cache{
-		Capacity:  2,
-		HashMap:   make(map[string]*CacheNode, 2),
-		firstNode: nil,
-		lastNode:  nil,
-	}
-
-	actualCache := cache
+	actualCache.Set("0", "a")
+	actualCache.Set("1", "b")
+	actualCache.Clear()
 
 	assert.Equal(t, expectedCache, actualCache)
+}
+
+var testLrucacheTestSuite = []struct {
+	name          string
+	capacity      int
+	expectedCache *Cache
+	expectedError error
+}{
+	{"BasicFuntionality", 1, createCache(1), nil},
+	{"No Capacity", 0, createCache(0), errors.New("capacity must be greater than zero")},
 }
 
 func TestLRUCache(t *testing.T) {
-	capacity := 1
-
-	expectedCache := &Cache{
-		Capacity:  1,
-		HashMap:   make(map[string]*CacheNode, 1),
-		firstNode: nil,
-		lastNode:  nil,
+	for _, testCase := range testLrucacheTestSuite {
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+			actualCache, actualError := LRUCache(testCase.capacity)
+			assert.Equal(t, testCase.expectedError, actualError)
+			assert.Equal(t, testCase.expectedCache, actualCache)
+		})
 	}
-
-	var expectedError error
-
-	actualCache, actualError := LRUCache(capacity)
-
-	assert.Equal(t, expectedError, actualError)
-	assert.Equal(t, expectedCache, actualCache)
-}
-
-func TestLRUCache_NoCapacity(t *testing.T) {
-	capacity := 0
-
-	var expectedCache *Cache
-
-	expectedError := errors.New("capacity must be greater than zero")
-
-	actualCache, actualError := LRUCache(capacity)
-
-	assert.Equal(t, expectedError, actualError)
-	assert.Equal(t, expectedCache, actualCache)
 }
